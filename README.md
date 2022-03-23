9a4e270f9f71a61ecda0bb6270ab975071efc2fc
katroll <kroll256@GMAIL.COM>
2022-03-22 21:22:45 -0400

    refactored  arabic to roman algorithm

---
+++ README.md
@@ -0,0 +1,21 @@
+# Roman Numeral Conversion App
+
+The purpose of this app is the allow users to enter either a roman numeral or an integer and convert it to the other.
+
+****
+
+Technologies:
+* Nextjs
+* Nodejs
+
+****
+
+To run the app: 
+
+1. Clone the repo to your local disc.
+
+2. Run `npm install` in root directory.
+
+3. Run `npm run dev` in root directory.
+
+4. open http://localhost:3000 in your browser.
--- pages/api/convert.js
+++ pages/api/convert.js
@@ -1,17 +1,47 @@
 
+function arabicToRoman(number) {
+    let roman = "";
 
-function arabicToRoman (num) {
-  var digits = String(+num).split(""),
-      key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-             "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-             "","I","II","III","IV","V","VI","VII","VIII","IX"],
-      roman = "",
-      i = 3;
-  while (i--)
-      roman = (key[+digits.pop() + (i * 10)] || "") + roman;
-  console.log(Array(+digits.join("") + 1).join("M") + roman)
-  return Array(+digits.join("") + 1).join("M") + roman;
+    const levels = [
+        {unit: 1000, symbol: "M", symbolHalfAbove: "K", symbolAbove: "K", symbolHalf: "D"},
+        {unit: 100, symbol: "C", symbolHalfAbove: "D", symbolAbove: "M", symbolHalf: "L"},
+        {unit: 10, symbol: "X", symbolHalfAbove: "L", symbolAbove: "C", symbolHalf: "V"},
+        {unit: 1, symbol: "I", symbolHalfAbove: "V", symbolAbove: "X", symbolHalf: "K"}
+    ]
+
+    for( let i = 0; i < 4; i++) {
+        const level = levels[i];
+
+        const units = Math.floor(number / level.unit);
+        if(units) {
+            if(units === 4) {
+                roman = roman.concat(`${level.symbol}${level.symbolHalfAbove}`);
+                number -= level.unit * units;
+            } else if(units === 9) {
+                roman = roman.concat(`${level.symbol}${level.symbolAbove}`);
+                number -= level.unit * units;
+            } else {
+                roman = roman.concat(`${level.symbol}`.repeat(units));
+                number -= level.unit * units;
+            }
+        }
+
+        console.log(level.unit, number);
+
+        if(level.unit != 1) {
+          const unitHalves = Math.floor(number / (level.unit / 2));
+          if(unitHalves && Math.floor(number - (level.unit / 2) < 4)) {
+              roman = roman.concat(`${level.symbolHalf}`.repeat(unitHalves));
+              number -= (level.unit / 2) * unitHalves;
+          }
+        }    
+
+    }
+
+    return roman;
 }
+
+
 
 const conversions = {"M": 1000, "D":500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1};
 
